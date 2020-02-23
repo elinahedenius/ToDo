eb53eb7485620e49105918bb09ae5ed1f14f140f
elinahedenius <bohedenius@protonmail.com>
2020-02-23 23:10:55 +0100

    made a lot of stuff that I don't remember

--- __main__.py
+++ __main__.py
@@ -1,63 +1,84 @@
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+#from kivy.app import App
+#from kivy.uix.gridlayout import GridLayout
+#from kivy.uix.label import Label
+#from kivy.uix.textinput import TextInput
+import requests
 
 listOfTasks = []
 listOfLists = []
+listOfParentLists = []
 
-class tasks():
+listCount = 0
 
-    def __init__(task, name, due, list):
-        task.name = name
-        task.due = due
-        task.list = list
+class Tasks():
 
-    def makeNewTask():
-        newTaskName = input('Task name: ')
-        newTaskDate = input('Task due date: ')
+    def __init__(self, name, due):
+        self.name = name
+        self.due = due
 
-        print('These are your available lists: ')
+    def __str__(self):
+        return self.name + "ska vara klart" + self.due
+
+def makeNewTask():
+    newTaskName = input('Task name: ')
+    newTaskDate = input('Task due date: ')
+
+    newTask = Tasks(newTaskName, newTaskDate)
+    listOfTasks.append(newTask)
+
+    print('These are your available lists: ')
+    for x in listOfLists:
+        print(x[0])
+    newTaskList = input('Which one do you want to put your new task in? (type none for none): ')
+    if newTaskList == 'none':
+        return;
+    else:
         for x in listOfLists:
-            print(x.name)
-        newTaskList = input('Which one do you want to put your new task in? (type none for none): ')
+            x.append(newTask)
+
+class List(list ):
+    def __init__(self, name):
+        self.name = name
+
+def makeNewList():
+    global listCount
+    name = input('Name of new list: ')
+    newList = List(name)
+    parentList = input('Put this list into this list (type none for none): ')
+    if parentList == 'none':
+        listOfParentLists.append(newList)
+    elif parentList != 'none':
         for x in listOfLists:
-            if newTaskList in x.name:
-                x.append(newTask)
-            elif newTaskList not in x.name:
-                createNewList = input('That is not a list. Do you want to create it? (y/n)')
-                if createNewList == 'y':
-                    newListList = ('Put into this list as a sublist (type none for none): ')
-                    lists.makeNewList()
-                    print('You have sucessfully added ' + newTaskName + ' to the list ' + newTaskList + '.')
-                elif createNewList == 'n':
-                    print('ok!')
-                    continue
-
-        newTask = tasks(newTaskName, newTaskDate, newTaskList)
-        listOfTasks.append(newTask)
-
-
-class lists():
-    def __init__(list, name, listToBeIn):
-        list.name = name
-        list.list = listToBeIn
-
-    def makeNewList():
-        newListName = input('List name: ')
-        newListList = input('Put into this list as a sublist (type none for none): ')
-
-        newList = list()
-        newList = lists(newListName, newListList)
-
-        listOfLists.append(newList)
-
+            if x.name == parentList:
+                x.append(newList)
+    newList.append(name)
+    listOfLists.append(newList)
+    listCount = listCount + 1
+    return newList
 
 while True:
-    choice = input('What do you want to do (create new task -> ct, create new list -> cl)')
+    print('These are your current lists: ')
+    for x in listOfParentLists:
+        print(x.name + ':')
+        for a in x:
+            if a == x.name:
+                continue
+            else:
+                print('     ' + a.name)
+
+    choice = input('What do you want to do? (see "help" for options) ')
 
     if choice == 'ct':
-        tasks.makeNewTask()
+        makeNewTask()
 
     elif choice == 'cl':
-        lists.makeNewList()
+        makeNewList()
+
+    elif choice == 'exit':
+        break
+
+    elif choice == 'help':
+        print (' cl → create new list \n ct → create new task \n exit → exit program')
+
+    else:
+        print('that is not a valid command :/')
